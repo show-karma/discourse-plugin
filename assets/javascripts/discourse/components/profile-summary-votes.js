71e0c219d9186ae9b41d954333e8aa1f2598076a
mury12 <jamesandre79@hotmail.com>
2022-08-18 16:20:26 -0300

    stylçe: eslint and prettier build errors

--- assets/javascripts/discourse/components/profile-summary-votes.js
+++ assets/javascripts/discourse/components/profile-summary-votes.js
@@ -1,8 +1,7 @@
 import Component from "@ember/component";
 import { inject as service } from "@ember/service";
-import { action, set } from "@ember/object";
+import { set } from "@ember/object";
 import VotingHistory from "../../lib/voting-history";
-import template from "../../lib/voting-history/template";
 
 export default Component.extend({
   router: service(),
