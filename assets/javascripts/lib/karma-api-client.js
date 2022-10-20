feab943e8b64af83673acda90cd3d77e9ba0f455
mury12 <jamesandre79@hotmail.com>
2022-10-20 15:31:49 -0300

    stage api for delegate pitch

--- assets/javascripts/lib/karma-api-client.js
+++ assets/javascripts/lib/karma-api-client.js
@@ -1,9 +1,9 @@
-import { karmaApiUrl } from "./consts";
+import { stageApiUrl } from "./consts";
 import { isTypeof } from "./is-typeof";
 import { request } from "./request";
 
 const localApi = "/karma-score";
-const karmaUrl = karmaApiUrl;
+const karmaUrl = stageApiUrl;
 class KarmaApiClient {
   daoName;
   publicAddress;
